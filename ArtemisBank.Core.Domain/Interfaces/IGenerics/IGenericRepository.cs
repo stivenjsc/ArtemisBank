92394664a3866a733c6ce8df2b87ea7c288f3046
Ana Z Santana G <20242153@itla.edu.do>
2026-03-11 00:18:03 -0400

    Add Interfaces and Igeneric

---
+++ ArtemisBank.Core.Domain/Interfaces/IGenerics/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtemisBank.Core.Domain.Interfaces.IGenerics
+{
+    public interface IGenericRepository<Entity> where Entity : class
+    {
+        Task<Entity> GetByIdAsync(int id);
+        Task<IEnumerable<Entity>> GetAllAsync();
+        Task AddAsync(Entity entity);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(Entity entity);
+    }
+}
