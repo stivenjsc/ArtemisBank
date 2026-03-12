680d1531f6b04067c2f0f70ce87fba35646d45e6
Ana Z Santana G <20242153@itla.edu.do>
2026-03-12 18:39:03 -0400

    The configuration for the database is added to the JSON file, and the database itself is added.

--- ArtemisBank.Infrastructure.Persistence/Context/ArtemisBankDbContext.cs
+++ ArtemisBank.Infrastructure.Persistence/Context/ArtemisBankDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace ArtemisBank.Infrastructure.Persistence.Context
+﻿using ArtemisBank.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+namespace ArtemisBank.Infrastructure.Persistence.Context
 {
-    public class ArtemisBankDbContext
+    public class ArtemisBankDbContext(DbContextOptions<ArtemisBankDbContext> options) : DbContext(options)
     {
+        public DbSet<Beneficiary> Beneficiaries { get; set; }
+        public DbSet<Commerce> Commerces { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<CreditCardConsumption> Consumptions { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<LoanInstallment> Installments { get; set; }
+        public DbSet<SavingsAccount> Savings { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+            mb.HasDefaultSchema("artemisBank");
+            mb.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
