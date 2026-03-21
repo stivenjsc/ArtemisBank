cb49c45a6000ba150a756946829ca015af903b58
Ana Z Santana G <20242153@itla.edu.do>
2026-03-21 02:33:58 -0400

    minor changes added at the program. adding automapper searching

--- ArtemisBank/Program.cs
+++ ArtemisBank/Program.cs
@@ -1,4 +1,5 @@
 using ArtemisBank.Core.Application.IoC;
+using ArtemisBank.Core.Application.Mappings;
 using ArtemisBank.Infrastructure.Identity;
 using ArtemisBank.Infrastructure.Identity.Entities;
 using ArtemisBank.Infrastructure.Identity.Seeds;
@@ -9,32 +10,41 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddApplicationLayer();
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 builder.Services.AddSharedInfrastructure(builder.Configuration);
 
+builder.Services.AddApplicationLayer();
+builder.Services.AddAutoMapper(cfg => { }, typeof(AutoMapperProfile));
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-
-    await DefaultRoles.SeedAsync(roleManager);
-    await DefaultUsers.SeedAsync(userManager);
+    try
+    {
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        await DefaultRoles.SeedAsync(roleManager);
+        await DefaultUsers.SeedAsync(userManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error during initial data seeding");
+    }
 }
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Login");
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
