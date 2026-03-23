7d3a9a00f6ff80433a18cb063342ddfcc4cd7d24
Ana Z Santana G <20242153@itla.edu.do>
2026-03-23 15:10:44 -0400

    changes dbname for de identity

---
+++ Presentation/WebApi/ArtemisBank.WebAPI/Controllers/v1/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using ArtemisBank.Core.Application.Interfaces.IServices;
+using Asp.Versioning;
+
+namespace ArtemisBank.WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class AccountController : BaseApiController
+    {
+        private readonly IUserService userService;
+    }
+}
