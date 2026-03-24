bcb91655276851af303d54d68636e0adfd018a0d
Ana Z Santana G <20242153@itla.edu.do>
2026-03-24 07:45:03 -0400

    Fixing something customer-related for the nineth time

--- ArtemisBank.Core.Application/ViewModels/Beneficiary/BeneficiaryViewModel.cs
+++ ArtemisBank.Core.Application/ViewModels/Beneficiary/BeneficiaryViewModel.cs
@@ -1,13 +1,14 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ArtemisBank.Core.Application.ViewModels.Beneficiary
 {
-    public class SaveBeneficiaryViewModel
+    public class BeneficiaryViewModel
     {
         [Required(ErrorMessage = "El número de cuenta es requerido.")]
         [StringLength(9, ErrorMessage = "El número de cuenta no es válido.", MinimumLength = 9)]
         public string AccountNumber { get; set; } = string.Empty;
         public string Alias { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
         public bool HasError { get; set; }
         public string? Error { get; set; }
     }
