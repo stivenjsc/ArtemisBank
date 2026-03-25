2de1fde24ae5f8de42106d3d56dda6edbebd9b47
Juan Celedonio <celedonioectiversom@gmail.com>
2026-03-24 23:55:47 -0400

    cashier account validation and activation email link base url

--- ArtemisBank/Program.cs
+++ ArtemisBank/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.ConfigureApplicationCookie(options =>
 {
